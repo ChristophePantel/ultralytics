Correcting issues with tensor dtype during BCE computation.

ultralytics/models/yolo/detect/val.py
@@ -326,7 +326,7 @@ class DetectionValidator(BaseValidator):
         if batch["cls"].shape[0] == 0 or preds["cls"].shape[0] == 0:
             return {"tp": np.zeros((preds["cls"].shape[0], self.niou), dtype=bool)}
         iou = box_iou(batch["bboxes"], preds["bboxes"])
-        bce = self.scores_bce(batch["scores"], preds["scores"])
+        bce = self.scores_bce(batch["scores"].to(device=preds["scores"].device,dtype=preds["scores"].dtype), preds["scores"])
         # TODO (CP/IRIT): preds["cls"] values must be adapted to many class prediction
         return {"tp": self.match_predictions(preds["cls"], batch["cls"], iou, bce).cpu().numpy()}
 