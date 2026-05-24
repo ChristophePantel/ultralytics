b3a7f158a417dc621e5750a57d916e6db0c3adb6
Christophe Pantel <ChristophePantel@users.noreply.github.com>
2026-05-24 12:27:47 +0200

    Commenting and verifying loss function.

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -380,24 +380,30 @@ class KnowledgeBasedLoss(nn.Module):
     # origin_indexes : class indexes for the domain
     # targets_from_source : class indexes for the co-domain
     def disjunction_loss(self, pred_scores, targets_from_source, power=3.0):
+        # O = batch_size x anchor_point_size
         batch_size, anchor_point_size, class_number = pred_scores.shape
+        # S
         sources = list(targets_from_source )
         source_number = len(sources)
+        # indexed by s in S
         predicate_s = torch.zeros(source_number)
+        # for each s in S
         for idx_s, s in enumerate(sources):
+            # t in T(s)
             targets_from_s = list(targets_from_source[s])
-            # p_c(o): indexed by o for a given s
+            # p_s(o): indexed by o for a given s
             source_scores = pred_scores[:, :, s]
-            # p_d(o)
+            # T(s)
             indexes = torch.tensor(targets_from_s,device=pred_scores.device)
+            # p_t(o): indexed by o in O and by t in T(s) for a given s in S
             target_scores = pred_scores.index_select( 2, indexes)
-            # max(p_d(o))
+            # max(p_t(o)) for t in T(s) 
             target_scores_max = torch.amax(target_scores,2)
             # p_s(o) - p_s(o) * max(p_t(o)): indexed by s and o
             predicate_s_o = source_scores - source_scores * target_scores_max
-            # p-mean for o in O
+            # p-mean for o in O for a given s in S, o (dim 1) is defined for each image in a batch (dim 0)
             predicate_s[idx_s] = torch.pow(predicate_s_o,power).mean(dim=(0,1))
-        # p-mean for s in sources
+        # p-mean for s in S
         result = torch.pow(torch.mean(predicate_s),1/power)
         return result
     
@@ -408,11 +414,12 @@ class KnowledgeBasedLoss(nn.Module):
     def conjunction_loss(self, pred_scores, targets_from_source, power=3.0):
         # O = batch_size x anchor_point_size
         batch_size, anchor_point_size, class_number = pred_scores.shape
-        sources = list(targets_from_source )
         # S
+        sources = list(targets_from_source )
         source_number = len(sources)
         # indexed by s in S
         predicate_s = torch.zeros(source_number)
+        # for each s in S
         for idx_s, s in enumerate(sources):
             # p_s(o): for a given s in S indexed by o in O
             source_scores = pred_scores[:,:,s:s+1]
@@ -424,7 +431,7 @@ class KnowledgeBasedLoss(nn.Module):
             target_scores_t = pred_scores.index_select( 2, indexes)
             # p_s(o) - p_s(o) * p_t(o): for a given s indexed by t and o
             predicate_s_t_o = source_scores - source_scores * target_scores_t
-            # p-mean for o in O: indexed by t in T(s)
+            # p-mean for o in O: indexed by t in T(s) for a given s in S, o (dim 1) is defined for each image in a batch (dim 0)
             predicate_s_t = torch.pow(predicate_s_t_o,power).mean(dim=(0,1))
             # p-mean for t in T(s)
             predicate_s[idx_s] = torch.mean(predicate_s_t)
@@ -437,11 +444,14 @@ class KnowledgeBasedLoss(nn.Module):
     # sources : class indexes for the domain
     # targets_from_source : class indexes for the co-domain
     def exclusion_loss(self, pred_scores, targets_from_source, power=3.0):
+        # O = batch_size x anchor_point_size
         batch_size, anchor_point_size, class_number = pred_scores.shape
+        # S
         sources = list(targets_from_source )
         source_number = len(sources)
         # indexed by s in S
         predicate_s = torch.zeros(source_number)
+        # for each s in S
         for idx_s, s in enumerate(sources):
             # p_s(o) for a given s in S
             source_scores = pred_scores[:,:,s:s+1]
@@ -451,18 +461,20 @@ class KnowledgeBasedLoss(nn.Module):
             # indexed by t in T(s) for a given s in S
             predicate_s_t = torch.zeros(s_targets_number)
             if s_targets_number > 1:
+                # for each t in T(s) for a given s in S
                 for idx_t, t in enumerate(targets_from_s):
-                    # T(s) \ { t } for a given s in S and t in T(s)
+                    # T(s) \ { t } for a given t in T(s) and s in S
                     targets_except_t = targets_from_s.copy()
                     targets_except_t.remove(t)
-                    # p_t(o) for a given t
+                    # p_t(o) for a given t in T(s) and s in S
                     target_scores_t = pred_scores[:,:, t:t+1]
-                    # p_e(o) for a given t in T(s) and s in S
                     indexes = torch.tensor(targets_except_t,device=pred_scores.device)
+                    # p_e(o) for a given e in T(s) \ { t } and t in T(s) and s in S
                     target_scores_e = pred_scores.index_select( 2, indexes)
-                    # p_t(o) * p_e(o) 
+                    # p_t(o) * p_e(o) indexed by o in O and e in T(s) \ { t } and t in T(s) and s in S
+                    # TODO (CP/IRIT): Check the appropriate dimension and size
                     predicate_s_t_e_o = target_scores_t * target_scores_e
-                    # p-mean for o in O
+                    # p-mean for o in O: indexed by e in T(s) \ { t } and t in T(s) for a given s in S, o (dim 1) is defined for each image in a batch (dim 0)
                     predicate_s_t_e = torch.pow(predicate_s_t_e_o,power).mean(dim=(0,1))
                     # p-mean on e in T(s) \ { t }
                     predicate_s_t[idx_t] = torch.mean(predicate_s_t_e)
